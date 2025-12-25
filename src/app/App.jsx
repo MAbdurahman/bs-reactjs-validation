a576566b45624a8c714be5dba8fa2c936b69e239
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-12-25 05:27:32 -0600

    Add basic structure for routes for Auth/SignIn and Auth/SighUp

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,18 @@
+import React, {Fragment} from 'react';
+import {Routes, Route} from 'react-router-dom';
+import SignUp from '../Auth/SignUp/index.jsx';
+import SignIn from '../Auth/SignIn/index.jsx';
+
 export default function App() {
 
    return (
-      <div>
-         <h2>App</h2>
-      </div>
+      <Fragment>
+         <Routes>
+            <Route path='/' element={<SignIn/>}/>
+            <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+         </Routes>
+      </Fragment>
 
    );
 }
