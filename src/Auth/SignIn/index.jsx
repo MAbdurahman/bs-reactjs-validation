b4f01a386f0927614ef5d1f0ef16a3460a36253f
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-12-25 06:22:02 -0600

    Improve basic structure for /Auth/SignIn and /Auth/SignUp

--- src/Auth/SignIn/index.jsx
+++ src/Auth/SignIn/index.jsx
@@ -4,14 +4,37 @@ import './styles.css';
 
 
 export default function SignIn() {
+   const [isShowing, setIsShowing] = useState(false);
    const [formData, setFormData] = useState({
       email: '',
       password: '',
    });
 
+   const handleChange = (e) => {
+      const {name, value} = e.target;
+      setFormData({...formData, [name]: value});
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+   };
+
    return (
-      <section className='sign_in'>
-         <h2 className='sign_in__title'>Sign In</h2>
+      <section className='sign__in'>
+         <h3 className='sign__in__title'>Sign In</h3>
+         <form action='' onSubmit={handleSubmit} className='sign__in__form'>
+            <div className='sign__in__form--group'>
+               <label htmlFor='email'>Email</label>
+               <input type='text' id='email' name='email' onChange={handleChange} />
+            </div>
+            <div className='sign__in__form--group'>
+               <label htmlFor='password'>Password</label>
+               <input type='password' id='password' name='password' onChange={handleChange}/>
+            </div>
+            <button type='submit' className='sign__in__form--button'>Sign In</button>
+            <p className='sign__in__form--text'>Do not have an account? <Link to='/sign-up'>Sign Up</Link></p>
+         </form>
       </section>
 
    );
--- src/Auth/SignUp/index.jsx
+++ src/Auth/SignUp/index.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import './styles.css';
 
 export default function SignUp() {
+   const [isShowing, setIsShowing] = useState(false);
    const [formData, setFormData] = useState({
       username: '',
       fullname: '',
@@ -10,9 +11,40 @@ export default function SignUp() {
       password: '',
    });
 
+   const handleChange = (e) => {
+      const {name, value} = e.target;
+      setFormData({...formData, [name]: value});
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formData);
+   };
+
+
    return (
       <section className='sign_up'>
-         <h2 className='sign_up__title'>Sign Up</h2>
+         <h3 className='sign_up__title'>Sign Up</h3>
+         <form action='' className='sign_up__form' onSubmit={handleSubmit}>
+            <div className='sign__up__form--group'>
+               <label htmlFor='username'>Username</label>
+               <input type='text' id='username' name='username' onChange={handleChange}/>
+            </div>
+            <div className='sign__up__form--group'>
+               <label htmlFor='fullname'>Full name</label>
+               <input type='text' id='fullname' name='fullname' onChange={handleChange}/>
+            </div>
+            <div className='sign__up__form--group'>
+               <label htmlFor='email' id='email'>Email</label>
+               <input type='email' id='email' name='email' onChange={handleChange}/>
+            </div>
+            <div className='sign__up__form--group'>
+               <label htmlFor='password' id='password'>Password</label>
+               <input type='password' id='password' name='password' onChange={handleChange}/>
+            </div>
+            <button type='submit' className='sign__up__form--button'>Sign Up</button>
+            <p className='sign__up__form--text'>Already have an account? <Link to='/sign-in'>Sign In</Link></p>
+         </form>
       </section>
 
    );
